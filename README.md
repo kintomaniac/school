2244a73ecfc65bf71675a5f2183c99cc6e149733
Kiran <kiran.tambe@vertisinfotech.com>
2015-03-19 13:22:31 +0530

    Removed .idea folder, added filter on standard.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# school
+## Synopsis
+
++ Displays student information.
+
+## Installation
+
++ Clone this repository:
+
+        git clone https://github.com/kintomaniac/school.git
+
++ Install requirements:
+
+        cd school
+        pip install -r requirements.txt
--- static/js/app.js
+++ static/js/app.js
@@ -3,18 +3,38 @@ var app = angular.module('school',[]);
 app.controller('StudentCtrl', ['$scope', '$http', function($scope, $http){
     var apiurl = '/api/student';
     $scope.objectList = [];
+    $scope.standardList=['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
     $scope.form={
         item:{}
     };
+    $scope.filterForm={};
 
-    $http.get(apiurl).success(function(response){
-        $scope.objectList = response.objects;
-    });
+    //Filter
+    var q={
+        filters:[]
+    };
 
+    //Get Student list
+    $scope.getStudents = function() {
+        if($scope.filterForm.standard){
+            q.filters = [{
+                name : "student_standard",
+                op : "==",
+                val : $scope.filterForm.standard
+            }];
+        }
+        $http.get(apiurl + '?q=' + JSON.stringify(q)).success(function (response) {
+            $scope.objectList = response.objects;
+        });
+    }
+
+    //Add new student
     $scope.add = function(){
         $http.post(apiurl, $scope.form.item).success(function(response){
             $scope.objectList.push(response);
             $scope.form.item={};
         });
     }
+
+    $scope.getStudents();
 }]);
